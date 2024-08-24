984cabaac31244d2f6ad2b47f023905410355122
Nirob <nowshad.nirob@gmail.com>
2024-08-24 22:54:06 +0600

    almost complete

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    container: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        '2xl': '1440px',
+      }
+    },
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      }
+    }
+  }
+}
